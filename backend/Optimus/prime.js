7baa0be093a679138a3ea0b2137e908366d400e0
Jav <j.a.cabrerapuente@gmail.com>
2020-05-14 12:46:55 -0600

    diagrama de bloques added

--- backend/Optimus/prime.js
+++ backend/Optimus/prime.js
@@ -21,12 +21,16 @@ class Prime {
                     // aqui optimizo el codigo
                     let lines = parser.parse(text);
                     let prime = [];
+                    let b = this.blocks(lines, true, null);
+                    this.blocksDot(b)
                     lines = this.rule1(lines, true);
                     lines = this.rule2(lines, true);
                     lines = this.rule3(lines, true);
                     lines = this.rules4n5(lines, true);
                     lines = this.rules6n7(lines, true);
+                    // lines = this.rule19(lines, true);
                     lines = this.rule20(lines, true);
+                    // lines = this.rule23(lines, true, null);
                     prime.push(lines[0].print()); // temporales
                     prime.push('var p, h;');
                     prime.push('var heap[];')
@@ -391,6 +395,243 @@ class Prime {
         return lines;
     }
 
+    rule21(lines, flag) {
+        for (let i = 0; i < lines.length; i++) {
+            if (flag) {
+                flag = false;
+                continue;
+            }
+
+            let current = lines[i];
+            // solo pueden usarse de forma valida los que el lado izquierdo no aparece en el lado derecho
+        }
+    }
+
+    rule22(lines, flag) {}
+
+    rule23(lines, flag, container) {
+        let wholeA = [];
+        let wholeP = [];
+        let wholeC = [];
+        if (flag) {
+            for (let i = 1; i < lines.length; i++) {
+                if (lines[i].getTypeOf() === 'asignment') {
+                    wholeA.push(lines[i]);
+                }
+                else if (lines[i].getTypeOf() === 'print') {
+                    wholeP.push(lines[i]);
+                }
+                else if (lines[i].getTypeOf() === 'conditional') {
+                    wholeC.push(lines[i]);
+                }
+                else if (lines[i].getTypeOf() === 'proc') {
+                    for (let j = 0; j < lines[i].instructions.length; j++) {
+                        let ins = lines[i].instructions[j];
+                        if (ins.getTypeOf() === 'asignment') {
+                            wholeA.push(ins);
+                        }
+                        else if (ins.getTypeOf() === 'print') {
+                            wholeP.push(ins);
+                        }
+                        else if (ins.getTypeOf() === 'conditional') {
+                            wholeC.push(ins);
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            wholeA = container.wholeA;
+            wholeP = container.wholeP;
+            wholeC = container.wholeC;
+        }
+        
+
+        for (let i = 0; i < lines.length; i++) {
+            if (flag) {
+                flag = false;
+                continue;
+            }
+
+            let current = lines[i];
+            if (current == null) {
+                continue;
+            }
+            if (current.getTypeOf() === 'asignment') {
+                if (current.left.type === 1 && current.left.value != 'p' && current.left.value != 'h') {
+                    let name = current.left.value;
+                    let ocurrencies = wholeA.filter(el => el.right.arg1 == name || el.right.arg2 == name);
+                    if (ocurrencies.length > 0) {
+                        continue;
+                    }
+                    ocurrencies = wholeP.filter(el => el.value == name);
+                    if (ocurrencies.length > 0) {
+                        continue;
+                    }
+                    ocurrencies = wholeC.filter(el => el.condition.arg1 == name || el.condition.arg2 == name);
+                    if (ocurrencies.length > 0) {
+                        continue;
+                    }
+                    lines[i] = null;
+                }
+            }
+            else if (current.getTypeOf() === 'proc') {
+                current.instructions = this.rule23(current.instructions, false, { wholeA: wholeA, wholeP: wholeP, wholeC: wholeC });
+            }
+        }   
+        lines = lines.filter(val => val != null);
+        return lines;
+    }
+
+    rule24(lines, flag) {} 
+
+    blocks(lines, flag, id) {
+        // un bloque tiene las instrucciones y etiqueta del bloque al que saltaria y su etiqueta propia
+        let blocks = [];
+        let runner = '';
+        let runLabel = id;
+        let runJump = null;
+        if (flag) {
+            runLabel = 'node0';
+            runner = 'var temps_list\nvar p, h;\nvar heap[];\nvar stack[];';
+        }
+
+        for (let i = 0; i < lines.length; i++) {
+            if (flag) {
+                flag = false;
+                continue;
+            }
+            let current = lines[i];
+            if (current.getTypeOf() === 'conditional') {
+                let lab = runLabel == null ? blocks.length : runLabel;
+                runJump = current.label;
+                if (runner == '') {
+                    blocks.push( { instructions: current.print(), label: lab, jump: runJump, elseJump: blocks.length+1});
+                }
+                else {
+                    blocks.push({instructions: runner, label: lab, jump: blocks.length + 1, elseJump: null});
+                    blocks.push({ instructions: current.print(), label: blocks.length, jump: runJump, elseJump: blocks.length+1 });
+                }
+                runner = '';
+                runLabel = null;
+                runJump = null;
+            }
+            else if (current.getTypeOf() === 'jump') {
+                let lab = runLabel == null ? blocks.length : runLabel;
+                runJump = current.label;
+                if (runner == '') {
+                    blocks.push( { instructions: current.print(), label: lab, jump: runJump, elseJump: null});
+                }
+                else {
+                    blocks.push({instructions: runner, label: lab, jump: blocks.length + 1, elseJump: null});
+                    blocks.push({ instructions: current.print(), label: blocks.length, jump: runJump, elseJump: null });
+                } 
+                runner = '';
+                runLabel = null;
+                runJump = null;
+            }
+            else if (current.getTypeOf() === 'destination') {
+                if (runner == '') {
+                    runLabel = current.label;
+                }
+                else {
+                    let lab = runLabel == null ? blocks.length : runLabel;
+                    blocks.push({ instructions: runner, label: lab, jump: current.label, elseJump: null });
+                    runLabel = current.label;
+                    runner = '';
+                }
+            }
+            else if (current.getTypeOf() === 'call') {
+                let lab = runLabel == null ? blocks.length : runLabel;
+                runJump = current.id;
+                if (runner == '') {
+                    blocks.push( { instructions: current.print(), label: lab, jump: runJump, elseJump: null});
+                }
+                else {
+                    blocks.push({instructions: runner, label: lab, jump: blocks.length + 1, elseJump: null});
+                    blocks.push({ instructions: current.print(), label: blocks.length, jump: runJump, elseJump: null });
+                } 
+                runner = '';
+                runLabel = null;
+                runJump = null;
+            }
+            else if (current.getTypeOf() === 'proc') {
+                // probably should call another method to take care of this
+                let b = this.blocks(current.instructions, false, current.id);
+                b.forEach(sq => {
+                    if (typeof(sq.jump) === 'number') {
+                        sq.jump = blocks.length + sq.jump - 1;
+                    }
+                    if (typeof(sq.label) === 'number') {
+                        sq.label = blocks.length;
+                    }
+                    if (typeof(sq.elseJump) === 'number') {
+                        sq.elseJump = blocks.length + sq.elseJump - 2;
+                    }
+                    blocks.push(sq);
+                });
+                blocks[blocks.length - 1].jump = blocks.length;
+                blocks.push({ instructions: 'end', label: blocks.length, jump: null, elseJump: null });
+            }
+            else {
+                if (current.getTypeOf() === 'print')
+                    runner += 'print\n';
+                else
+                    runner += current.print() + '\n';
+                if (i + 1 == lines.length) {
+                    let lab = runLabel == null ? blocks.length : runLabel;
+                    blocks.push( { instructions: runner, label: lab, jump: blocks.length, elseJump: null} );
+                }
+            }
+        }
+
+        return blocks;
+    }
+
+    blocksDot(blocks) {
+        let dot = [];
+        dot.push(`digraph G {\n node[shape=box]`);
+        // nodos
+        blocks.forEach(block => {
+            if (typeof(block.label) === 'number') {
+                dot.push(`node${block.label}[label="${block.instructions}"]`);
+                block.label = `node${block.label}`;
+            }
+            else {
+                dot.push(`${block.label}[label="${block.instructions}"]`);
+            }
+        })
+
+        blocks.forEach(block => {
+            if (block.jump != null) {
+                if (typeof(block.jump) === 'number') {
+                    dot.push(`${block.label}->node${block.jump}`);
+                }
+                else {
+                    dot.push(`${block.label}->${block.jump}`);
+                }
+            }
+
+            if (block.elseJump != null) {
+                if (typeof(block.elseJump) === 'number') {
+                    dot.push(`${block.label}->node${block.elseJump}`);
+                }
+                else {
+                    dot.push(`${block.label}->${block.elseJump}`);
+                }
+            }
+        })
+
+        dot.push('}');
+        // relaciones
+        let joined = dot.join('\n');
+        fs.writeFile('./Testing/blocks.dot', joined, (err, file) => {
+            if (err)
+                console.log(err);
+        });
+
+        return joined;
+    }
 }
 
 module.exports.Prime = Prime;
